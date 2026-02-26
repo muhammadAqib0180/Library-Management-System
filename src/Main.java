bdfe28e899d24b9f96fd4f319246b2c3a49b8515
M Aqib Buzdar <aqibbuzdar5@gmail.com>
2026-02-26 19:17:00 +0500

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main() {
+        System.out.println("System Check: Environment is configured correctly.");
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("Test: Sum of " + a + " and " + b + " is " + sum);
+    }
+}
